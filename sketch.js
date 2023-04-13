1ba9aa7985c04fd0111ee82bb47e2c77751fb9ec
MrDerman <danni_boi69@live.co.uk>
2023-04-13 10:44:18 +0100

    Tweaks to mouse arm curves

--- sketch.js
+++ sketch.js
@@ -1,47 +1,42 @@
-let imgMouthClosedEyesOpen,
-  imgMouthClosedEyesClosed,
-  imgMouthOpenEyesOpen,
-  imgMouthOpenEyesClosed;
-let imgKeyPressDown, imgKeyPressUp, imgBG, imgMouse;
-let blinking = false,
-  keyPress = false;
+const images = {};
+let blinking = false;
+let keyPress = false;
 let blinkDelay = 200;
 let tick = 0;
-let mousePosX = 0,
-  mousePosY = 0;
+let mousePosX = 0;
+let mousePosY = 0;
 const ws = new WebSocket("ws://localhost:9001");
 
-function preload() {
-  imgMouthClosedEyesOpen = loadImage("assets/1.png");
-  imgMouthClosedEyesClosed = loadImage("assets/2.png");
-  imgMouthOpenEyesOpen = loadImage("assets/3.png");
-  imgMouthOpenEyesClosed = loadImage("assets/3.png");
-  imgKeyPressDown = loadImage("assets/down.png");
-  imgKeyPressUp = loadImage("assets/up.png");
-  imgBG = loadImage("assets/bg.png");
-  imgMouse = loadImage("assets/mouse.png");
-}
-
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-}
 ws.onmessage = function (event) {
-  console.log(event.data)
   if (event.data.includes("KeyPress")) {
     keyPress = true;
   } else if (event.data.includes("KeyRelease")) {
     keyPress = false;
   }
-  if (event.data.includes("Hack:")) {
-    let tempPos = event.data.split(" ");
-    mousePosX = tempPos[1];
-    mousePosY = tempPos[2];
+  if (event.data.includes("MouseMove:")) {
+    const [_, x, y] = event.data.split(" ");
+    mousePosX = x;
+    mousePosY = y;
   }
   ws.send("readyformore!");
 };
 
+function preload() {
+  images.mouthClosedEyesOpen = loadImage("assets/1.png");
+  images.mouthClosedEyesClosed = loadImage("assets/2.png");
+  images.mouthOpenEyesOpen = loadImage("assets/3.png");
+  images.mouthOpenEyesClosed = loadImage("assets/4.png");
+  images.mouthOpenEyesClosed = loadImage("assets/down.png");
+  images.keyPressUp = loadImage("assets/up.png");
+  images.bg = loadImage("assets/bg.png");
+  images.mouse = loadImage("assets/mouse.png");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
 function draw() {
-  background(0, 255, 0);
   tick++;
   if (tick == blinkDelay - 10) {
     blinking = true;
@@ -50,43 +45,53 @@ function draw() {
     tick = 0;
     blinkDelay = Math.floor(random(150, 200));
   }
-  let img = blinking ? imgMouthClosedEyesClosed : imgMouthClosedEyesOpen;
-  let imgHand = keyPress ? imgKeyPressDown : imgKeyPressUp;
-  image(imgBG, 0, 0);
+
+  const img = blinking
+    ? images.mouthClosedEyesClosed
+    : images.mouthClosedEyesOpen;
+  const imgHand = keyPress ? images.mouthOpenEyesClosed : images.keyPressUp;
+
+  const mouseOffsetX = 140;
+  const mouseOffsetY = 260;
+  const scaledMouseY = (mousePosY - 240) / (209 - 240) + mouseOffsetY;
+  const scaledMouseX = (mousePosX - 240) / (209 - 240) + mouseOffsetX;
+
+  background(0, 255, 0);
+  image(images.bg, 0, 0);
   image(img, 0, 0);
   image(imgHand, 0, 0);
-  let mouseOffsetX = 140,
-    mouseOffsetY = 260;
-  let ScaledMouseY = (mousePosY - 240) / (209 - 240) + mouseOffsetY,
-    ScaledMouseX = (mousePosX - 240) / (209 - 240) + mouseOffsetX;
+  image(images.mouse, scaledMouseX - 35, scaledMouseY - 25);
 
-  image(imgMouse, ScaledMouseX - 35, ScaledMouseY - 25);
-  let fillColour = color("#6abd8f"),
-    strokeColour = color("#847669");
+  const fillColour = color("#6abd8f");
+  const strokeColour = color("#847669");
+
   fill(fillColour);
   stroke(fillColour);
   strokeWeight(1);
-  triangle(250, 170, 173, 140, ScaledMouseX, ScaledMouseY);
+
+  triangle(225, 170, 200, 119, scaledMouseX, scaledMouseY);
 
   strokeWeight(6);
   stroke(strokeColour);
+
   curve(
-    -100 + ScaledMouseX * 5, //c1
-    ScaledMouseY / 3, //c1
-    250, //x1
+    -400 + scaledMouseX * 5, //c1
+    scaledMouseY / 3, //c1
+    225, //x1
     170, //y1
-    ScaledMouseX, //x2
-    ScaledMouseY, //y2
+    scaledMouseX, //x2
+    scaledMouseY, //y2
     0, //c2
     0 //c2
   );
+
   curve(
-    -300 + ScaledMouseX * 5, //c1
-    ScaledMouseY / 3, //c1
-    173, //x1
-    140, //y1
-    ScaledMouseX, //x2
-    ScaledMouseY, //y2
+    -100 + scaledMouseX * 5, //c1
+    scaledMouseY / 3, //c1
+    200, //x1
+    119, //y1
+    scaledMouseX, //x2
+    scaledMouseY, //y2
     300, //c2
     200 //c2
   );
